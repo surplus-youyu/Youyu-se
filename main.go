eabbfe1e749e4bfb0bb8d6e6f8e70f51ea8737b2
buchuitoudegou <756541536@qq.com>
2019-04-29 14:14:24 +0800

    register route

--- main.go
+++ main.go
@@ -1,22 +1,14 @@
 package main
 
-//import "github.com/gin-gonic/gin"
-//
-//func main() {
-//	r := gin.Default()
-//	r.GET("/ping", func(c *gin.Context) {
-//		c.JSON(200, gin.H{
-//			"message": "pong",
-//		})
-//	})
-//	r.Run() // listen and serve on 0.0.0.0:8080
-//}
 import (
-	//"fmt"
-	"github.com/surplus-youyu/Youyu-se/models"
+	"github.com/gin-gonic/gin"
+	"github.com/surplus-youyu/Youyu-se/route"
 )
-
 func main() {
-	models.GetUserByUid("123")
-	//fmt.Println(result)
+	r := gin.Default()
+	route.Route(r)
+	err := r.Run() // listen and serve on 0.0.0.0:8080
+	if err != nil {
+		panic(err)
+	}
 }
