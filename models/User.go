7c36428d20a58f84baa745281d3fe9c1011c7f3f
buchuitoudegou <756541536@qq.com>
2019-04-30 16:24:18 +0800

    feat: register

--- models/User.go
+++ models/User.go
@@ -1,15 +1,11 @@
 package models
 
-import (
-	"fmt"
-)
-
 type User struct {
-	Uid      string  `gorm:"column:uid"`
+	Uid      int     `gorm:"column:uid"`
 	Password string	 `gorm:"column:password"`
 	RealName string  `gorm:"column:real_name"`
 	NickName string  `gorm:"column:nick_name"`
-	Age      string  `gorm:"column:age"`
+	Age      int     `gorm:"column:age"`
 	Gender   string  `gorm:"column:gender"`
 	Balance  float32 `gorm:"column:balance"`
 	Major    string  `gorm:"column:major"`
@@ -18,9 +14,12 @@ type User struct {
 	Email    string  `gorm:"column:email"`
 }
 
-func GetUserByEmail(email string) User {
-	result := User{}
+func GetUserByEmail(email string) []User {
+	var result []User
 	DB.Table("user").Where("email=?", email).Find(&result)
-	fmt.Println(result)
 	return result
+}
+
+func CreateNewUser(newUser User) {
+	DB.Table("user").Create(&newUser)
 }
