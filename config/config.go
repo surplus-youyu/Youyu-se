161d9703f11dec13aa68fb1ba64f05ac314c3087
Veg_chic <h_foryou@163.com>
2019-05-10 00:23:08 +0800

    hide config

--- config/config.go
+++ config/config.go
@@ -1,8 +1,10 @@
 package config
 
-const (
-	DBHost = "193.112.59.226:3306"
-	DBName = "xftest"
-	DBUser = "root"
-	DBPwd  = "980325"
+import "os"
+
+var (
+	DBHost = os.Getenv("DBHost")
+	DBName = os.Getenv("DBName")
+	DBUser = os.Getenv("DBUser")
+	DBPwd  = os.Getenv("DBPwd")
 )
